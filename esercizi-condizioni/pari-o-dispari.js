6e918290ea6f949e69d5fb4eaec746139b9aba5b
Stefano <bluestefan77@gmail.com>
2018-02-20 19:14:58 +0100

    solution pari o dispari

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -3,3 +3,15 @@
   Scrivi un programma che prenda in input un numero intero e restituisca 0 se è pari o 1 se è dispari.
   http://www.imparareaprogrammare.it
 */
+/*
+definisco la variabile
+identifico se è pari o dispari
+definisco un ciclo if-else per il risultato finale
+*/
+var n=5;
+
+if (n%2 == 0) {
+  console.log(0);
+} else {
+console.log(1);
+}

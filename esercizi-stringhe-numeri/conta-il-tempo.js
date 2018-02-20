61d923ab7edbbd37b6db853bb122c2baaea5f439
Stefano <bluestefan77@gmail.com>
2018-02-20 17:21:09 +0100

    solution conta il tempo
    my solution

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,18 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+
+/*
+numero di secondi=3800
+numero di ore = numero di secondi/3600 arrotondato a intero inferiore
+numero di minuti = numero di secondi avanzati/60 arrotondato a intero inferiore
+numero di secondi = numero di secondi avanzati
+stampa in output il risultato
+*/
+
+var seconds = 3800;
+var hours = Math.floor(seconds/3600);
+var minutes = Math.floor((seconds-(hours*3600))/60);
+var finalseconds = seconds - 60*minutes - 3600*hours;
+
+console.log(seconds+' seconds '+'are exactly '+ hours+ ' hours '+minutes + ' minutes and '+finalseconds+' seconds');

13d9c87ef7370787447b7698a8af882fa14ef2f3
Stefano <bluestefan77@gmail.com>
2018-02-21 09:34:15 +0100

    solution fai il professore  - IF

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -13,3 +13,18 @@
   Realizzare una versione con il costrutto Switch-Case.
   http://www.imparareaprogrammare.it
 */
+var v = 23;
+
+if (v < 18) {
+  console.log('Il voto è insufficiente');
+} else if (v >= 18 && v <21) {
+  console.log('Il voto è sufficiente');
+} else if (v >= 21 && v < 24) {
+  console.log('Il voto è buono');
+} else if (v >= 24 && v < 27) {
+  console.log('Il voto è distinto');
+} else if (v >= 27 && v < 29) {
+  console.log('Il voto è ottimo');
+} else {
+  console.log('Il voto è eccellente');
+}

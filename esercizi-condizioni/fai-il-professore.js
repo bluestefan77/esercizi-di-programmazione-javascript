0ebb1a6587a6b9a306b7791c3e072f576c92b03c
Stefano <bluestefan77@gmail.com>
2018-02-21 09:53:31 +0100

    solution fai il professore - Switch

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -13,18 +13,46 @@
   Realizzare una versione con il costrutto Switch-Case.
   http://www.imparareaprogrammare.it
 */
-var v = 23;
+var v = 20;
 
-if (v < 18) {
+switch (v) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
   console.log('Il voto è insufficiente');
-} else if (v >= 18 && v <21) {
+  break;
+  case 18:
+  case 19:
+  case 20:
+  case 21:
   console.log('Il voto è sufficiente');
-} else if (v >= 21 && v < 24) {
+  break;
+  case 22:
+  case 23:
+  case 24:
   console.log('Il voto è buono');
-} else if (v >= 24 && v < 27) {
+  break;
+  case 25:
+  case 26:
+  case 27:
   console.log('Il voto è distinto');
-} else if (v >= 27 && v < 29) {
+  case 28:
+  case 29:
   console.log('Il voto è ottimo');
-} else {
+  case 30:
   console.log('Il voto è eccellente');
-}
+  }

d283735d3ad471337b6a02870320b0ad294c34b9
Stefano <bluestefan77@gmail.com>
2018-02-21 09:16:14 +0100

    solution che giorno oggi

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -12,3 +12,36 @@
   Scriverne una versione che anziché i giorni della settimana, visualizzi i nomi dei mesi.
   http://www.imparareaprogrammare.it
 */
+var dayOfTheWeek = 5;
+
+switch (dayOfTheWeek) {
+  case 1:
+  dayOfTheWeek='lunedì';
+  break;
+  case 2:
+  dayOfTheWeek='martedì';
+  break;
+  case 3:
+  dayOfTheWeek='mercoledì';
+  break;
+  case 4:
+  dayOfTheWeek='giovedì';
+  break;
+  case 5:
+  dayOfTheWeek='venerdì';
+  break;
+  case 6:
+  dayOfTheWeek='sabato';
+  break;
+  case 7:
+  dayOfTheWeek='domenica';
+  break
+  default:
+  dayOfTheWeek='NaD';
+}
+
+if (dayOfTheWeek != 'NaD') {
+  console.log('Hai selezionato il ' + dayOfTheWeek);
+} else {
+  console.log('Il numero immesso non è un giorno della settimana');
+}

64d7f8b1ed520f115e343de4a6f29487b8936ed6
Stefano <bluestefan77@gmail.com>
2018-02-21 09:03:23 +0100

    solution chi l'azzecca

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -5,3 +5,29 @@
   Hint: Per generare un numero casuale utlizza la funzione javascript random, che restituisce un intervallo compreso tra 0 e 1 che necessita di essere convertito per il tuo intervallo.
   http://www.imparareaprogrammare.it
 */
+/*pesco dall'urna il mio numeretto utilizzando la funzione random e moltiplicando per 100
+prendo numero 1 e numero 2 e li confronto con il mio numeretto. Se non sono uguali faccio la sottrazione per vedere quale dei due è più vicino
+*/
+var casualNumber = Math.round(100 * Math.random());
+console.log('il numero casuale è ' + casualNumber)
+var firstNumber = 13;
+console.log('il primo numero è ' + firstNumber)
+var secondNumber = 62;
+console.log('il secondo numero è ' + secondNumber)
+
+var first = Math.abs(casualNumber-firstNumber);
+var second = Math.abs(casualNumber-secondNumber);
+var min=Math.min(first,second);
+
+if (min == 0) {
+  if (first == 0);
+  console.log('Il primo numero è uguale a quello casuale')
+} else if (second == 0) {
+  console.log('il secondo numero è uguale a quello casuale');
+} else {
+  if (min == first) {
+  console.log('Il primo numero è più vicino a quello casuale')
+} else if (min == second){
+  console.log('il secondo numero è più vicino a quello casuale');
+}
+}

7a60a0c04cf949157144fe9753851927e661c4a6
Stefano <bluestefan77@gmail.com>
2018-02-22 14:23:54 +0100

    solution ti ricordi le tabelline

--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -3,3 +3,8 @@
   Scrivi un programma che stampi che prenda in input un numero e stampi la tabellina corrispondente.
   http://www.imparareaprogrammare.it
 */
+
+var n = 4;
+for (var i=0; i < 10; i++) {
+console.log(n*i);
+}

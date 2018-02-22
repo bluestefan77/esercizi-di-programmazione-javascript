3922190863120ea4f0c30f8527ef240f87dd36bb
Stefano <bluestefan77@gmail.com>
2018-02-22 15:04:14 +0100

    solution vinci tu

--- esercizi-cicli/vinci-tu.js
+++ esercizi-cicli/vinci-tu.js
@@ -4,3 +4,21 @@
   Stampa il vincitore che ha totalizza più punti.
   http://www.imparareaprogrammare.it
 */
+/*Tiro il dado N volte, quindi lo faccio da 0 a N. La mia variabile si incrementa fino al numero di tiri deciso
+i++ significa "fai un altro tiro"
+Per ogni tiro ottengo un risultato che va immagazzinato in una variabile punteggio ed infine le N variabili punteggio vanno a sommarsi per
+ottenere il totale. Tutto ciò va iterato due volte che è il numero di giocatori
+*/
+
+var players = 2;
+var tiri = 3;
+var somma=0;
+
+for (var j=1; j<=players; j++) {
+  for(var i=0; i<tiri; i++) {
+    var dado = (Math.ceil((Math.random()*60)/10));
+    var somma = somma + dado;
+  }
+  console.log('il giocatore numero ' + j + ' ha totalizzato ' + somma);
+  somma = 0;
+}

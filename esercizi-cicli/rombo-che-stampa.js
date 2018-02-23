d4e8152cf4ec648c27385c7c21f1992c4cbcc7e9
Stefano <bluestefan77@gmail.com>
2018-02-23 17:59:56 +0100

    Solution rombo che stampa

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -11,3 +11,31 @@
   Hint: per indentare puoi usare il tab con '\t'.
   http://www.imparareaprogrammare.it
 */
+/*
+Sulla prima riga lascio uno spazio formato da (n-1)/2 buchi poi una X ed ancora lo stesso spazio
+sulla seconda riga lascio uno spazio formato da ((n-1)/2 -1) buchi poi una X e via
+Quando il risultato dell'operazione è uguale ad n inverto il trend.
+Non è dato sapere in anticipo quante iterazioni ci saranno perché non so il numero
+che l'utente andrà ad inserire, per cui utilizzo un ciclo while invece di un ciclo for
+*/
+var n = 7; /*rappresenta il numero di input */
+var i = 1;
+var empty = ' ';
+var full = 'X';
+var emptySpace;
+var fullSpace;
+
+while (i < n) {
+  var j  = ((n - i) / 2);
+  fullSpace = full.repeat(i);
+  emptySpace = empty.repeat(j);
+  console.log(emptySpace + fullSpace + emptySpace);
+  i = i+2;
+}
+while (i >= 1) {
+  var j  = Math.abs((n - i) / 2);
+  fullSpace = full.repeat(i);
+  emptySpace = empty.repeat(j);
+  console.log(emptySpace + fullSpace + emptySpace);
+  i = i-2;
+}

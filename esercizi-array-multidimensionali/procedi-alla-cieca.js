af01c6f04956e6b57260347024dd2e0b6ca77215
Stefano <bluestefan77@gmail.com>
2018-02-26 15:20:46 +0100

    Solution procedi alla cieca

--- esercizi-array-multidimensionali/procedi-alla-cieca.js
+++ esercizi-array-multidimensionali/procedi-alla-cieca.js
@@ -4,3 +4,14 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var array1 = [];
+var array2 = [];
+
+for (var i = 0; i < 10; i++) {
+  for (var j = 0 ; j < 10; j++) {
+  array2[j] = Math.round(Math.random()*10);
+}
+array1[i] = array2;
+}
+console.log('La matrice è: ' + array1);

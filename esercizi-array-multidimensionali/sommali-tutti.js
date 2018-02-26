7384d81f16b94dc44ca2f3a9e1d77a98e0d73790
Stefano <bluestefan77@gmail.com>
2018-02-26 19:42:43 +0100

    solution sommali tutti

--- esercizi-array-multidimensionali/sommali-tutti.js
+++ esercizi-array-multidimensionali/sommali-tutti.js
@@ -4,8 +4,8 @@
     - Prenda in input due numeri, N e M.
     - Generi una matrice di NxM popolata di valori casuali da 1 a 100.
     - Stampi la matrice e la somma di tutti i valori contenuti.
-  
-    Input : 
+
+    Input :
       2 3
     Output:
       [
@@ -16,3 +16,23 @@
 
   http://www.imparareaprogrammare.it
 */
+var N = 3;
+var M = 4;
+var matrice = [];
+var array = [];
+var somma = 0;
+
+for (var i = 0; i < N; i++) {
+    for (var j = 0; j < M; j++) {
+      array[j] = Math.ceil(Math.random()*100);
+    }
+  matrice[i]=array;
+  }
+console.log(matrice);
+
+for (var i = 0; i < N; i++) {
+    for (var j = 0; j < M; j++) {
+    somma += matrice[i][j];
+    }
+  }
+console.log(somma);

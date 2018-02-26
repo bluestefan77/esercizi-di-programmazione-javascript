02be8ac3ea5a516b6e9a79dd708ec85fe4d1b145
Stefano <bluestefan77@gmail.com>
2018-02-26 15:06:39 +0100

    Solution metti un poco di ordine

--- esercizi-array/metti-un-poco-di-ordine.js
+++ esercizi-array/metti-un-poco-di-ordine.js
@@ -6,3 +6,20 @@
   Prova ad ordinali in modo crescente.
   http://www.imparareaprogrammare.it
 */
+
+var array = [];
+
+for (var i = 0; i < 10; i++) {
+array[i] = Math.ceil(Math.random()*10);
+}
+console.log('Primo array: ' + array);
+
+array.sort(function(a, b) {
+  return b-a;
+});
+console.log('L\' array ordinato in ordine decrescente è: ' + array);
+
+array.sort(function(a, b) {
+  return a-b;
+});
+console.log('L\' array ordinato in ordine crescente è: ' + array);

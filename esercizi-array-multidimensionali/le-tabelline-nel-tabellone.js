19d0dc2abbe5fd93658eeff6da8ff4e08560f5ff
Stefano <bluestefan77@gmail.com>
2018-02-26 16:02:24 +0100

    solution le tabelline nel tabellone

--- esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
+++ esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
@@ -8,6 +8,17 @@
     2 | 0 | 2 | 4 | 6 | 8 | 10 | 12 | 14 | 16 | 18 | 20
     ...
     10 | 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100
-    
+
   http://www.imparareaprogrammare.it
 */
+
+var array1 = [0,1,2,3,4,5,6,7,8,9,10];
+var array2 = [];
+var array3 = [];
+for (var i = 0; i <= 10; i++) {
+  for (var j = 0; j <= 10; j++) {
+  array2[j] = array1[i]*array1[j];
+  array3[j] = (array2[j] +'/');
+}
+console.log(array1[i] + '/' + array3);
+}

38321af9475ab4bd180ed583b0738c200d5cb600
Stefano <bluestefan77@gmail.com>
2018-02-28 17:01:34 +0100

    solution la funzione banale

--- esercizi-funzioni/la-funzione-banale.js
+++ esercizi-funzioni/la-funzione-banale.js
@@ -3,3 +3,11 @@
   Scrivi una funzione di uguaglianza che prenda in input due argomenti e restituisca 'Vero' se i due argomenti sono identici, altrimenti 'Falso'.
   http://www.imparareaprogrammare.it
 */
+var N=2;
+var M=3;
+
+function banale (x,y) {
+  return x==y;
+}
+
+console.log(banale(N,M));

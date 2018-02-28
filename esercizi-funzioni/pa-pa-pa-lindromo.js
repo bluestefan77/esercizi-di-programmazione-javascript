0a5f03db5e7abbf478190fabbc454b85f1ad783d
Stefano <bluestefan77@gmail.com>
2018-02-28 18:12:58 +0100

    solution pa-pa-pa-lindromo

--- esercizi-funzioni/pa-pa-pa-lindromo.js
+++ esercizi-funzioni/pa-pa-pa-lindromo.js
@@ -8,3 +8,14 @@
   Hint: puoi eliminare spazi e segni di punteggiatura usando le espressioni regolarie e il metodo del tipo stringa chiamato replace, in questo modo: str.replace(/\W/g, "")
   http://www.imparareaprogrammare.it
 */
+
+var parola ='ai lati d\'italia';
+var parolaPulita = parola.replace(/\W/g, "");
+console.log(parolaPulita);
+
+function palindroma(x) {
+  return [parolaPulita.split("").reverse().join("") == parolaPulita, parolaPulita.split("").reverse().join("")];
+  //converto in array, inverto e riconverto in stringa, poi in output produco un array dove vedo andhe il risultato dell'inversione
+}
+
+console.log(palindroma(parola));

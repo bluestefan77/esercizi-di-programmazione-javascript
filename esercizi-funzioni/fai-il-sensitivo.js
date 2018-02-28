1c9edaddc6f2139703cfb4ba639c0bcc61f2eee7
Stefano <bluestefan77@gmail.com>
2018-02-28 18:22:42 +0100

    solution fai il sensitivo

--- esercizi-funzioni/fai-il-sensitivo.js
+++ esercizi-funzioni/fai-il-sensitivo.js
@@ -1,11 +1,30 @@
 /*
   Fai il sensitivo
-  Scrivi una funzione che calcoli la vicinanza tra tre argomenti numerici, A, B e N, e restituisca: 
+  Scrivi una funzione che calcoli la vicinanza tra tre argomenti numerici, A, B e N, e restituisca:
     - 0 Se A e B sono equidistanti da N
     - 1 Se B è più vicino a N rispetto ad A
     - -1 Se A è più vicino a N rispetto a B: restituis
-    
+
   Variante
   Utilizza questa funzione per creare un programma che simuli un gioco tra due utenti: vince chi indovina per primo il numero N che verrà generato casualmente tra 1 e 100.
   http://www.imparareaprogrammare.it
 */
+
+var A = 4;
+var B = 12;
+var N = 8;
+
+function distanzaNumerica(x,y,z) {
+  var distanza = Math.abs(x-z);
+  var output;
+  if (distanza < Math.abs(y-z)) {
+    output=-1;
+  } else if (distanza == Math.abs(y-z)) {
+    output=0;
+  } else {
+    output=1;
+  }
+  return output;
+}
+
+console.log(distanzaNumerica(A,B,N));

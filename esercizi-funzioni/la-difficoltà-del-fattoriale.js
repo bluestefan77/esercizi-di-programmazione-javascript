7cbafc8ceedd3892d740fde556bb80ac639d457a
Stefano <bluestefan77@gmail.com>
2018-02-28 17:17:01 +0100

    solution la difficoltà del fattoriale

--- esercizi-funzioni/la-difficoltà-del-fattoriale.js
+++ esercizi-funzioni/la-difficoltà-del-fattoriale.js
@@ -7,3 +7,16 @@
   Hint: 5! = 5*4*3*2*1
   http://www.imparareaprogrammare.it
 */
+
+var input = 5;
+var prodotto;
+
+function fattoriale (x) {
+prodotto = input;
+  for (var i = input; i > 1; i--) {
+    prodotto = prodotto * (i-1);
+  }
+  return prodotto;
+}
+
+console.log(fattoriale(input));

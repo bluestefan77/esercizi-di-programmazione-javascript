14d58481e49cb97292ee52df67927f2fc741cb25
Stefano <bluestefan77@gmail.com>
2018-02-28 17:38:33 +0100

    solution la bellezza della casualità

--- esercizi-funzioni/la-bellezza-della-casualità.js
+++ esercizi-funzioni/la-bellezza-della-casualità.js
@@ -3,3 +3,12 @@
   Scrivi una funzione che prenda in input un numero e restituisca un numero casuale compreso tra 0 e l'argomento passato.
   http://www.imparareaprogrammare.it
 */
+
+var input = 52;
+
+function casuale (x) {
+  num = Math.round(Math.random()*input);
+  return num;
+}
+
+console.log(casuale(input));

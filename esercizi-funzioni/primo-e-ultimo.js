e5417c959f35becc2de3c4153d7110c0459960af
Stefano <bluestefan77@gmail.com>
2018-02-28 17:06:08 +0100

    solution primo e ultimo

--- esercizi-funzioni/primo-e-ultimo.js
+++ esercizi-funzioni/primo-e-ultimo.js
@@ -6,3 +6,20 @@
   Scrivi due funzioni che prendano in input un array di numeri, una funzione deve restituire il valore maggiore contenuto nell'array, l'altra il valore minore.
   http://www.imparareaprogrammare.it
 */
+
+function maggiore (x,y,z) {
+  if (x > y) {
+    var maggiore = x;
+  } else {
+    maggiore = y;
+  }
+
+  if (maggiore > z) {
+    maggiore = maggiore;
+  } else {
+    maggiore = z;
+  }
+  return maggiore;
+}
+
+console.log(maggiore(8,5,7));

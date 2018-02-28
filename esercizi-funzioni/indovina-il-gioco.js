c4a595000bcbf6c0585167d349231a7c7ce270fc
Stefano <bluestefan77@gmail.com>
2018-02-28 17:28:13 +0100

    solution indovina il giorno

--- esercizi-funzioni/indovina-il-gioco.js
+++ esercizi-funzioni/indovina-il-gioco.js
@@ -11,3 +11,36 @@
   Nel caso il giorno non sia compreso nel range, il programma dovrà restituire un messaggio d'errore simile a questo: 'Peccato, non posso indovinare il giorno.'
   http://www.imparareaprogrammare.it
 */
+
+var input=3;
+
+function giorno(x) {
+  return x>=1 && x<=7;
+}
+console.log(giorno(input));
+
+switch (input) {
+    case 1:
+    console.log('Lunedì');
+    break;
+    case 2:
+    console.log('Martedì');
+    break;
+    case 3:
+    console.log('Mercoledì');
+    break;
+    case 4:
+    console.log('Giovedì');
+    break;
+    case 5:
+    console.log('Venerdì');
+    break;
+    case 6:
+    console.log('Sabato');
+    break;
+    case 7:
+    console.log('Domenica');
+    break;
+  default:
+  console.log('Non è un giorno della settimana.');
+}

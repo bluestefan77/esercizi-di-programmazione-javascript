95182f81841fe165fa480d4def842e0a0db98a71
Stefano <bluestefan77@gmail.com>
2018-03-01 11:39:59 +0100

    solution i primi numeri primi

--- esercizi-funzioni/i-primi-numeri-primi.js
+++ esercizi-funzioni/i-primi-numeri-primi.js
@@ -11,3 +11,37 @@
             11
   http://www.imparareaprogrammare.it
 */
+var numero = 11;
+var array = [];
+var resto = [];
+
+//Funzione che definisce se un certo valore è un numero Primo
+//Restituisce true se è numero primo, false in caso contrario
+function numeriPrimi(x) {
+  for (var i = 2; i < x; i++) {
+    resto[i] = x%i;
+}
+  var index = resto.indexOf(0);;
+  return index==-1;
+}
+console.log(numeriPrimi(numero));
+
+//Applico la funzione di cui sopra a tutti i numeri da 1 fino ad un valore incognito
+//Genero un array di valori dove i numeri NON primi hanno assegnato il valore 0
+//Elimino gli zeri dall'array e prendo solo i numeri primi che mi interessano
+//cioè tanti quanti sono prima del numero assegnato all'inizio
+var j = 0;
+while (array.length < numero) {
+  j++;
+    if (numeriPrimi(j) == true) {
+    array[j] = j;
+  } else {
+    array[j] = 0;
+}
+}
+
+var filtered = array.filter(function eliminaGliZeri(element){
+  return (element != 0);
+});
+
+console.log(filtered);

ea63110d41931cd1e1b48afd2858ceeedaf580ac
Stefano <bluestefan77@gmail.com>
2018-03-04 22:07:11 +0100

    solution la mia rubrica

--- esercizi-oggetti/la-mia-rubrica.js
+++ esercizi-oggetti/la-mia-rubrica.js
@@ -13,16 +13,56 @@
 
   Variante
   Migliora i metodi di Modifica e Cancellazione, al posto di prendere in input l'indice dell'array passa in input il nome e ricava l'indice sul quale applicare l'operazione.
-
+  Gestore quindi un array di oggetti prevedendo la possibileà di: inserire, modificare, visualizzare, ricercare e cancellare contatti della rubrica.
   http://www.imparareaprogrammare.it
 */
 
+var rubrica = [contatto1, contatto2];
 
+var contatto1 = {
+  nome: 'Stefano',
+  numero: '123456789',
+  indirizzo: 'Lofoten straße'
+}
+var contatto2 = {
+  nome: 'Ivana',
+  numero: '987654321',
+  indirizzo: 'Lofoten straße'
+}
 
+function visualizzaLista (x) {
+for (var i = 0; i < x.length; i++) {
+  console.log(x[i].nome);
+}
+}
+visualizzaLista(rubrica);
 
+function inserisciContatto (x) {
+  x.push({
+    nome: 'Pippo',
+    numero: '123457777',
+    indirizzo: 'Plutonia'
+  })
+}
+inserisciContatto(rubrica);
+console.log(rubrica);
 
+function visualizzaContatto (x) {
+  console.log((rubrica[x]));
+}
+visualizzaContatto(1);
 
+function eliminaContatto (x) {
+  rubrica.splice(x,1);
+}
+eliminaContatto(1);
+console.log(rubrica);
 
-
-
-Gestore quindi un array di oggetti prevedendo la possibileà di: inserire, modificare, visualizzare, ricercare e cancellare contatti della rubrica.
+function cercaNome (x) {
+  for (var i = 0; i < rubrica.length; i++) {
+  if (rubrica[i].nome == x) {
+    console.log(rubrica[i]);
+  }
+}
+}
+cercaNome('Stefano');
